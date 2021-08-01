e4a6fee41f00ec06bba95038c759501d72e32738
mickeycohen <cohen.mickey@gmail.com>
2021-08-01 13:29:26 +0300

    Change bracket styles and set locale to US

--- src/main/java/com/mickeyc/helloworld/HelloWorld.java
+++ src/main/java/com/mickeyc/helloworld/HelloWorld.java
@@ -2,13 +2,14 @@ package com.mickeyc.helloworld;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
-public class HelloWorld {
-
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+public class HelloWorld 
+{
+  public static void main(final String[] args) 
+  {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'").withLocale(Locale.US);
     final LocalDateTime now = LocalDateTime.now();
-
     System.out.println("Hello, World!");
     System.out.println("The current time is " + dtf.format(now));
     System.out.println("Goodbye");
