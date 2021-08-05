4657a87750874c638fcd519fa470ba5f7b63ca6d
mickeycohen <cohen.mickey@gmail.com>
2021-08-05 09:00:20 +0300

    adding hostname

--- src/main/java/com/mickeyc/helloworld/HelloWorld.java
+++ src/main/java/com/mickeyc/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@ package com.mickeyc.helloworld;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class HelloWorld 
 {
@@ -12,6 +14,15 @@ public class HelloWorld
     final LocalDateTime now = LocalDateTime.now();
     System.out.println("Hello, World!");
     System.out.println("The current time is " + dtf.format(now));
+    try
+    {
+      String localHostname = InetAddress.getLocalHost().getHostName();
+      System.out.println("Running on host " + localHostname);
+    }
+    catch (UnknownHostException ex)
+    {
+      System.out.println("Can't get on hostname!");
+    }    
     System.out.println("Goodbye");
   }
 }
